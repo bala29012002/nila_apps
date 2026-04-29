ee38c7b6d25d6565e6b296c645295bbb9a7528bc
JDS084-Balamurugan <balamurugan.s@jdsoft.in>
2026-04-29 23:29:57 +0530

    commit on 29-04-26(created path builder as maven project)

---
+++ adaptive_learning_path_builder/src/main/java/com/nila_apps/adaptive_learning_path_builder/AdaptiveLearningPathBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.nila_apps.adaptive_learning_path_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptiveLearningPathBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaptiveLearningPathBuilderApplication.class, args);
+	}
+
+}
---
+++ adaptive_learning_path_builder/src/test/java/com/nila_apps/adaptive_learning_path_builder/AdaptiveLearningPathBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nila_apps.adaptive_learning_path_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptiveLearningPathBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
